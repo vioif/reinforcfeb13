15dff07109b6074fd4005525418067cb61a69197
vioif <ireneaflores3@gmail.com>
2018-02-19 19:35:06 -0500

    completed exercise

---
+++ exercise.rb
@@ -0,0 +1,15 @@
+def draw_shape(options)
+  shape = ""
+
+  options[:rows].times do |r|
+    options[:cols].times do |c|
+      shape += options[:char]
+    end
+    shape += "\n"
+  end
+
+  return shape
+
+end
+
+puts draw_shape({rows: 3, cols: 7, char: "0"})
